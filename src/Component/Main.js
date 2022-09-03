5f9ebc5800071a4aea835ed75f82abc06f67ba26
TheJokeR1993 <thejokermafiaclub@gmail.com>
2022-09-03 22:20:52 +0300

    hashrout

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,4 +1,4 @@
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, HashRouter} from 'react-router-dom'
 import MainPage1 from './page1/MainPage1'
 import MainPage2 from './page2/MainPage2'
 import MainPage3 from './page3/MainPage3'
@@ -8,11 +8,11 @@ const Main = ()=>{
    
     return <div>
     <Routes>
-     <Route path="/minimo_1"  element={<MainPage1 /> } />
-    <Route path="/minimo_2"  element={<MainPage2 />} />
+     <HashRouter path="/1"  element={<MainPage1 /> } />
+    <HashRouter path="/2"  element={<MainPage2 />} />
    
-    <Route path="/minimo_3" element={<MainPage3 />} />
-     <Route path="/"  element={<MainPage1 /> } />
+    <HashRouter path="/3" element={<MainPage3 />} />
+     <HashRouter path="/"  element={<MainPage1 /> } />
     </Routes>
     </div>
    
